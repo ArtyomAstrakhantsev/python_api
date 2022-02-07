922cfaa9c685b3e64cc9f8c938d51d6281424265
ayastrak <ayastrak@stream.ru>
2022-02-07 16:50:17 +0300

    добавил исключение try/except

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+from simplejson.errors import JSONDecodeError
 import requests
 
 response = requests.get("https://playground.learnqa.ru/api/get_text")
 print(response.text)
+
+try:
+    parsed_json = response.json()
+    print(parsed_json)
+except JSONDecodeError:
+    print("Response is a not JSON")
