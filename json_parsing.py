40c1c28cd6d1c325703820b66153585a8ae2ccaf
ayastrak <ayastrak@stream.ru>
2022-02-07 17:56:32 +0300

    достаю второе сообщение из json

--- json_parsing.py
+++ json_parsing.py
@@ -1,11 +1,11 @@
 import json
 
-string_as_json_format = '{"answer": "Hello, Artyom"}'
+string_as_json_format = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
 obj = json.loads(string_as_json_format)
 
-key = "answer"
+mes = "messages"
 
-if key in obj:
-    print(obj[key])
+if mes in obj:
+    print(obj[mes][1])
 else:
-    print(f"Ключа {key} нет в json")
+    print(f"Ключа {mes} нет в json")
--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-from simplejson.errors import JSONDecodeError
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
-print(response.text)
-
-try:
-    parsed_json = response.json()
-    print(parsed_json)
-except JSONDecodeError:
-    print("Response is a not JSON")
+responce = requests.post("https://playground.learnqa.ru/api/check_type", data={"param1": "value1"})
+print(responce.text)
