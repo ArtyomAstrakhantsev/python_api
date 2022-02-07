537f91f5b1c6ccb9ab7fadd82f94fcdffba6f288
ayastrak <ayastrak@stream.ru>
2022-02-07 20:33:09 +0300

    исправил название переменной param на parameter

--- method.py
+++ method.py
@@ -2,22 +2,22 @@ import requests
 import json
 
 
-params = '[{"method": "GET"}, {"method": "POST"}, {"method": "DELETE"}, {"method": "PUT"}, {"method": "HEAD"}]'
-param = json.loads(params)
+parameters = '[{"method": "GET"}, {"method": "POST"}, {"method": "DELETE"}, {"method": "PUT"}, {"method": "HEAD"}]'
+parameter = json.loads(parameters)
 
 method = [requests.get, requests.post, requests.delete, requests.put, requests.head]
 url = 'https://playground.learnqa.ru/ajax/api/compare_query_type'
 
 x = 0
-for i in range(len(param)):
+for i in range(len(parameter)):
     for x in range(len(method)):
         print(i)
         if i == 0:
-            response = method[x](url, params=param[i])
+            response = method[x](url, params=parameter[i])
             print(response.text)
             x += 1
         else:
-            response = method[x](url, data=param[i])
+            response = method[x](url, data=parameter[i])
             print(response.text)
             x += 1
     i += 1
