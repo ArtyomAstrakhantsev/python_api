3c8326a14fc2de3fa9112c7e143d92dcf9f11721
ayastrak <ayastrak@stream.ru>
2022-02-18 16:37:07 +0300

    add logger in project

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,7 +1,7 @@
-import requests
 import pytest
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+from lib.my_requests import MyRequests
 
 
 class TestAuth(BaseCase):
@@ -15,7 +15,7 @@ class TestAuth(BaseCase):
             "email": "vinkotov@example.com",
             "password": "1234"
         }
-        self.response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+        self.response1 = MyRequests.post("/user/login", data=data)
         self.auth_sid = self.get_cookie(self.response1,"auth_sid")
         self.token = self.get_headers(self.response1, "x-csrf-token")
         self.user_id_from_auth_method = self.get_json_value(self.response1, "user_id")
@@ -23,8 +23,8 @@ class TestAuth(BaseCase):
     def test_auth(self):
         auth_sid = self.response1.cookies.get("auth_sid")
         token = self.response1.headers.get("x-csrf-token")
-        response2 = requests.get(
-            "https://playground.learnqa.ru/api/user/auth", 
+        response2 = MyRequests.get(
+            "/user/auth", 
             headers={"x-csrf-token": token},
             cookies={"auth_sid": auth_sid}
         )
@@ -38,13 +38,13 @@ class TestAuth(BaseCase):
     @pytest.mark.parametrize('condition', exclude_params)
     def test_negative_auth(self, condition):
         if condition == "no_cookie":
-            response2 = requests.get(
-                "https://playground.learnqa.ru/api/user/auth",
+            response2 = MyRequests.get(
+                "/user/auth",
                 headers={"x-csrf-token": self.token}
             )
         else:
-            response2 = requests.get(
-               "https://playground.learnqa.ru/api/user/auth",
+            response2 = MyRequests.get(
+               "/user/auth",
                 cookies={"auth_sid": self.auth_sid}
             )
         Assertions.assert_json_value_by_name(
--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,13 +1,14 @@
 import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+from lib.my_requests import MyRequests
 
 class TestUserRegister(BaseCase):
 
     def test_user_create_successfully(self):
         data = self.prepare_reg_data()
 
-        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
 
         Assertions.assert_json_have_key(response, "id")
         Assertions.assert_code_status(response, 200)
@@ -17,7 +18,7 @@ class TestUserRegister(BaseCase):
         email = "vinkotov@example.com"
         data = self.prepare_reg_data(email)
 
-        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
 
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
